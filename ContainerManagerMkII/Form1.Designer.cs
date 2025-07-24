ca91ff5f2759a3da635527b99b8a3da71c5ac105
LegendaryMercury <ljfastell@gmail.com>
2025-07-24 14:29:36 +1000

    Initial commit

---
+++ ContainerManagerMkII/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ContainerManagerMkII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ContainerListBox1 = new System.Windows.Forms.ListBox();
+            this.AddContainerButton = new System.Windows.Forms.Button();
+            this.ContainerNumberTxtBox = new System.Windows.Forms.TextBox();
+            this.ContainerNumbLable1 = new System.Windows.Forms.Label();
+            this.RemoveContainerButton = new System.Windows.Forms.Button();
+            this.ContainerWeightTxtBox = new System.Windows.Forms.TextBox();
+            this.ContainerWeightLable1 = new System.Windows.Forms.Label();
+            this.ContainerCapacityTxtBox = new System.Windows.Forms.TextBox();
+            this.ContainerCapLabel1 = new System.Windows.Forms.Label();
+            this.TwentyFCheckBox = new System.Windows.Forms.CheckBox();
+            this.FourtyFCheckBox = new System.Windows.Forms.CheckBox();
+            this.EditContainerButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ContainerListBox1
+            // 
+            this.ContainerListBox1.FormattingEnabled = true;
+            this.ContainerListBox1.Location = new System.Drawing.Point(12, 259);
+            this.ContainerListBox1.Name = "ContainerListBox1";
+            this.ContainerListBox1.Size = new System.Drawing.Size(776, 147);
+            this.ContainerListBox1.TabIndex = 1;
+            // 
+            // AddContainerButton
+            // 
+            this.AddContainerButton.Location = new System.Drawing.Point(209, 25);
+            this.AddContainerButton.Name = "AddContainerButton";
+            this.AddContainerButton.Size = new System.Drawing.Size(150, 25);
+            this.AddContainerButton.TabIndex = 2;
+            this.AddContainerButton.Text = "Add Container";
+            this.AddContainerButton.UseVisualStyleBackColor = true;
+            this.AddContainerButton.Click += new System.EventHandler(this.AddContainerButton_Click);
+            // 
+            // ContainerNumberTxtbox
+            // 
+            this.ContainerNumberTxtBox.Location = new System.Drawing.Point(12, 25);
+            this.ContainerNumberTxtBox.Name = "ContainerNumberTxtbox";
+            this.ContainerNumberTxtBox.Size = new System.Drawing.Size(191, 20);
+            this.ContainerNumberTxtBox.TabIndex = 3;
+            // 
+            // ContainerNumbLable1
+            // 
+            this.ContainerNumbLable1.AutoSize = true;
+            this.ContainerNumbLable1.Location = new System.Drawing.Point(12, 9);
+            this.ContainerNumbLable1.Name = "ContainerNumbLable1";
+            this.ContainerNumbLable1.Size = new System.Drawing.Size(92, 13);
+            this.ContainerNumbLable1.TabIndex = 4;
+            this.ContainerNumbLable1.Text = "Container Number";
+            // 
+            // RemoveContainerButton
+            // 
+            this.RemoveContainerButton.Location = new System.Drawing.Point(209, 56);
+            this.RemoveContainerButton.Name = "RemoveContainerButton";
+            this.RemoveContainerButton.Size = new System.Drawing.Size(150, 25);
+            this.RemoveContainerButton.TabIndex = 5;
+            this.RemoveContainerButton.Text = "Remove Container";
+            this.RemoveContainerButton.UseVisualStyleBackColor = true;
+            this.RemoveContainerButton.Click += new System.EventHandler(this.RemoveContainerButton_Click);
+            // 
+            // ContainerWeightTxtBox
+            // 
+            this.ContainerWeightTxtBox.Location = new System.Drawing.Point(12, 64);
+            this.ContainerWeightTxtBox.Name = "ContainerWeightTxtBox";
+            this.ContainerWeightTxtBox.Size = new System.Drawing.Size(191, 20);
+            this.ContainerWeightTxtBox.TabIndex = 6;
+            // 
+            // ContainerWeightLable1
+            // 
+            this.ContainerWeightLable1.AutoSize = true;
+            this.ContainerWeightLable1.Location = new System.Drawing.Point(12, 48);
+            this.ContainerWeightLable1.Name = "ContainerWeightLable1";
+            this.ContainerWeightLable1.Size = new System.Drawing.Size(110, 13);
+            this.ContainerWeightLable1.TabIndex = 7;
+            this.ContainerWeightLable1.Text = "Container Weight (kg)";
+            // 
+            // ContainerCapacityTxtBox
+            // 
+            this.ContainerCapacityTxtBox.Location = new System.Drawing.Point(12, 103);
+            this.ContainerCapacityTxtBox.Name = "ContainerCapacityTxtBox";
+            this.ContainerCapacityTxtBox.Size = new System.Drawing.Size(191, 20);
+            this.ContainerCapacityTxtBox.TabIndex = 8;
+            // 
+            // ContainerCapLabel1
+            // 
+            this.ContainerCapLabel1.AutoSize = true;
+            this.ContainerCapLabel1.Location = new System.Drawing.Point(9, 87);
+            this.ContainerCapLabel1.Name = "ContainerCapLabel1";
+            this.ContainerCapLabel1.Size = new System.Drawing.Size(118, 13);
+            this.ContainerCapLabel1.TabIndex = 9;
+            this.ContainerCapLabel1.Text = "Container Capacity (KL)";
+            // 
+            // TwentyFCheckBox
+            // 
+            this.TwentyFCheckBox.AutoSize = true;
+            this.TwentyFCheckBox.Location = new System.Drawing.Point(12, 142);
+            this.TwentyFCheckBox.Name = "TwentyFCheckBox";
+            this.TwentyFCheckBox.Size = new System.Drawing.Size(85, 17);
+            this.TwentyFCheckBox.TabIndex = 10;
+            this.TwentyFCheckBox.Text = "Twenty Foot";
+            this.TwentyFCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // FourtyFCheckBox
+            // 
+            this.FourtyFCheckBox.AutoSize = true;
+            this.FourtyFCheckBox.Location = new System.Drawing.Point(103, 142);
+            this.FourtyFCheckBox.Name = "FourtyFCheckBox";
+            this.FourtyFCheckBox.Size = new System.Drawing.Size(79, 17);
+            this.FourtyFCheckBox.TabIndex = 11;
+            this.FourtyFCheckBox.Text = "Fourty Foot";
+            this.FourtyFCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // EditContainerButton
+            // 
+            this.EditContainerButton.Location = new System.Drawing.Point(209, 87);
+            this.EditContainerButton.Name = "EditContainerButton";
+            this.EditContainerButton.Size = new System.Drawing.Size(150, 25);
+            this.EditContainerButton.TabIndex = 12;
+            this.EditContainerButton.Text = "Edit Container";
+            this.EditContainerButton.UseVisualStyleBackColor = true;
+            this.EditContainerButton.Click += new System.EventHandler(this.EditContainerButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Red;
+            this.ExitButton.ForeColor = System.Drawing.Color.DarkRed;
+            this.ExitButton.Location = new System.Drawing.Point(209, 118);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(150, 25);
+            this.ExitButton.TabIndex = 13;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.EditContainerButton);
+            this.Controls.Add(this.FourtyFCheckBox);
+            this.Controls.Add(this.TwentyFCheckBox);
+            this.Controls.Add(this.ContainerCapLabel1);
+            this.Controls.Add(this.ContainerCapacityTxtBox);
+            this.Controls.Add(this.ContainerWeightLable1);
+            this.Controls.Add(this.ContainerWeightTxtBox);
+            this.Controls.Add(this.RemoveContainerButton);
+            this.Controls.Add(this.ContainerNumbLable1);
+            this.Controls.Add(this.ContainerNumberTxtBox);
+            this.Controls.Add(this.AddContainerButton);
+            this.Controls.Add(this.ContainerListBox1);
+            this.Name = "Form1";
+            this.Text = "Container Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox ContainerListBox1;
+        private System.Windows.Forms.Button AddContainerButton;
+        private System.Windows.Forms.TextBox ContainerNumberTxtBox;
+        private System.Windows.Forms.Label ContainerNumbLable1;
+        private System.Windows.Forms.Button RemoveContainerButton;
+        private System.Windows.Forms.TextBox ContainerWeightTxtBox;
+        private System.Windows.Forms.Label ContainerWeightLable1;
+        private System.Windows.Forms.TextBox ContainerCapacityTxtBox;
+        private System.Windows.Forms.Label ContainerCapLabel1;
+        private System.Windows.Forms.CheckBox TwentyFCheckBox;
+        private System.Windows.Forms.CheckBox FourtyFCheckBox;
+        private System.Windows.Forms.Button EditContainerButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ ContainerManagerMkII/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ContainerManagerMkII.Form1.ShippingContainer;
+/*
+ * Program by Lachlan Astell
+ * This program is a container database, you are supposed to enter a container's details and then be able to use it for whatever.
+ * This program is for practice and will grow as i need it too.
+ * Date of creation 27/04/2025 11:29pm
+ */
+namespace ContainerManagerMkII
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Our container Object, Star of the show for now.
+        public class ShippingContainer
+        {
+            // Containers come in two sizes, so i created a custom variable for them.
+            public enum ContainerSize
+            {
+                Twentyfeet,
+                Fortyfeet
+            }
+            // Set variables for container
+            public string ContNumber { get; set; }
+            public int ContWeight { get; set; }
+            public double ContCapacity { get; set; }
+            public ContainerSize ContSize { get; set; }
+
+            // This is a constructor, it lets us quickly make new itterations of the object.
+            public ShippingContainer(string number, int weight, double capacity, ContainerSize size)
+            {
+                ContNumber = number;
+                ContWeight = weight;
+                ContCapacity = capacity;
+                ContSize = size;
+            }
+            // This allows us to generate the string output and tells the form, or list box in this case, how to put the output string together.
+            public override string ToString()
+            {
+                return $"Number: {ContNumber}, Weight: {ContWeight}kg, Capacity: {ContCapacity}kL, Size: {ContSize}";
+            }
+
+        }
+
+        //This function will validate the weight input.
+        private bool ValidateWeight(string input, out int weight)
+        {
+            weight = 0 ;
+            //Try to convert the input text to an integer value, if it fails return false.
+            if (!int.TryParse(input, out weight))
+            {
+                // In future try to combine message boxes errors.
+                MessageBox.Show("Please enter a valid WHOLE number for container weight");
+                return false ;
+            }
+            //Checks if the weight range is valid
+            // || is the OR operator in C and C#
+
+            if (weight < 500 || weight > 10000)
+            {
+                // In future try to combine message boxes errors.
+                MessageBox.Show("Weight must be between 500 and 10,000kg to be accepted.");
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateCapacity(string input, out double capacity)
+        {
+            capacity = 0 ;
+            //Tries to convert to a double value
+            if (!double.TryParse(input, out capacity))
+            {
+                // In future try to combine message boxes errors.
+                MessageBox.Show("Please enter a valid Decimal number please.");
+                return false;
+            }
+            if (capacity < 0 || capacity > 70)
+            {
+                // In future try to combine message boxes errors.
+                MessageBox.Show("Capacity must be between 0 and 70kg to be accepted.");
+                return false;
+            }
+
+            return true ;
+        }
+        private bool ValidateContainerNumber(string input, out string ErrorMessage)
+        {
+            ErrorMessage = "" ;
+
+            //Check the length of the number.
+            if(input.Length != 11)
+            {
+                ErrorMessage = "Error, number must be 11 characters";
+                return false ;
+            }
+
+            //Check if its unique.
+            foreach (var item in ContainerListBox1.Items)
+            {
+                if (item is ShippingContainer container)
+                {
+                    if (container.ContNumber == input)
+                    {
+                        ErrorMessage = "Error, that item already Exists";
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        private void AddContainerButton_Click(object sender, EventArgs e)
+        {
+            /* This function serves to add items to the list.
+             * When the button is clicked it will be added to the list of items.
+             * Items are added from the text boxes and attempted to be converted to their values.
+             * 
+             */
+
+            //Read the inputs from the text boxes.
+            string ContNum = ContainerNumberTxtBox.Text;
+            string numberError;
+            string ContWeightInput = ContainerWeightTxtBox.Text; //input for our checker
+            string ContCapacityInput = ContainerCapacityTxtBox.Text;
+            // string ContNumInput = "defaultnumber"; Old way of doing this.
+
+            int ContWeight = 0; // This will be the output variable for our checker
+            double ContCapacity = 0; // This too!
+
+            if (!ValidateContainerNumber(ContNum, out numberError))
+            {
+                MessageBox.Show(numberError, "Container Number Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //This will validate if our weight value is acceptable
+            if (!ValidateWeight(ContWeightInput, out ContWeight))
+            {
+                //Stop running code if failed
+                return;
+            }
+            //This will validate if our Capacity value is acceptable
+            if (!ValidateCapacity(ContCapacityInput, out ContCapacity))
+            {
+                //Stop running code if failed
+                return;
+            }
+
+            //Creates variable size for the shipping container to use locally.
+            ShippingContainer.ContainerSize Contsize = ShippingContainer.ContainerSize.Twentyfeet;
+
+            //Read numerical input from textbox, this works by trying to phrase the values i give it as int and double respectively.
+
+            int.TryParse(ContainerWeightTxtBox.Text, out ContWeight); // Not sure if i need this anymore
+
+            double.TryParse(ContainerCapacityTxtBox.Text,out ContCapacity); // Hello.
+
+            if (TwentyFCheckBox.Checked)
+            {
+                Contsize = ShippingContainer.ContainerSize.Twentyfeet;
+            }
+            else if (FourtyFCheckBox.Checked)
+            {
+                Contsize = ContainerSize.Fortyfeet;
+            }
+
+            //Create a new container object, the enum is used like this: 'ContainerSize.Twentyfeet' this sets it to a twenty footer.
+            ShippingContainer new_Container = new ShippingContainer(ContNum, ContWeight, ContCapacity, Contsize);
+
+            // List boxes work by first by specifying 'Items', then 'Add' or any other function next.
+            ContainerListBox1.Items.Add(new_Container);
+
+            // Clear the inputs after adding
+            ContainerNumberTxtBox.Clear();
+            ContainerWeightTxtBox.Clear();
+            ContainerCapacityTxtBox.Clear();
+            TwentyFCheckBox.Checked = false;
+            FourtyFCheckBox.Checked = false;
+        }
+
+        //Removes the selected item from the list of items
+        private void RemoveContainerButton_Click(object sender, EventArgs e)
+        {
+            if (ContainerListBox1.SelectedItem != null)
+            {
+                //This is the fancy version of the yes and no message box. If you select yes it will remove the selected item.
+                var result = MessageBox.Show("Are you sure you want to remove this container?", "Confirm Remove", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    //This line removes the selected item from the list
+                    ContainerListBox1.Items.Remove(ContainerListBox1.SelectedItem);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a container to remove.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //Edit the selected item by bringing it back to the text boxes
+        private void EditContainerButton_Click(object sender, EventArgs e)
+        {
+            if(ContainerListBox1.SelectedItem is ShippingContainer selectedContainer)
+            {
+                //Bring back to text boxes
+                ContainerNumberTxtBox.Text = selectedContainer.ContNumber;
+                ContainerWeightTxtBox.Text = selectedContainer.ContWeight.ToString();
+                ContainerCapacityTxtBox.Text = selectedContainer.ContCapacity.ToString();
+
+                if (selectedContainer.ContSize == ShippingContainer.ContainerSize.Twentyfeet)
+                {
+                    TwentyFCheckBox.Checked = true;
+                    FourtyFCheckBox.Checked = false;
+                }
+                else
+                {
+                    TwentyFCheckBox.Checked = false;
+                    FourtyFCheckBox.Checked = true;
+                }
+
+                //Remove item from the list of items so it can be re-added later
+                ContainerListBox1.Items.Remove(selectedContainer);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a container to edit");
+            }
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
